44d5bfa369b493017045beca013dd7c9419b48bf
Do25-ops <dataodyssey25@gmail.com>
2025-02-17 03:06:26 +0500

    Added frontend to CORS policy

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ app.use(cookieParser());
 app.use(authentication);
 
 app.use(cors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'], 
+    origin: ['https://frontend-d5zxv7tfq-do25-ops-projects.vercel.app','http://localhost:5173', 'http://127.0.0.1:5173'], 
     credentials: true,
   }));
 
