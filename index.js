c47ba6b56710f19f310cfd2d09e24ee2fe1dc37f
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-19 00:22:52 +0500

    reverted commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true })); 
 app.use(cookieParser());
 
-app.use(authentication);
+//app.use(authentication);
 
 
 app.use('/api',userRoutes);
