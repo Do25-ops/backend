31d87ed8470738b05a6d0eb47132219f75646675
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-18 19:28:24 +0500

    updated user controller

--- routes/competition.routes.js
+++ routes/competition.routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const {sendQueries,submitQuery, sendLeaderboardData, fetchStatus} = require('../controllers/competition.controller');
+const { fetchLevel } = require('../controllers/user.controller');
 
 router.get('/queries/:id',sendQueries)
 router.get('/leaderboardData',sendLeaderboardData)
 router.post('/submitFile',submitQuery);
 router.get('/getSubmissionStatus',fetchStatus);
+router.get('/fetchTeamLevel/:id',fetchLevel)
 
 
 module.exports = router;
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { loginUser, logoutUser, registerUser, retrieveCookie, getDashboard, markTutorialDone, fetchLevel } = require('../controllers/user.controller');
+const { loginUser, logoutUser, registerUser, retrieveCookie, getDashboard, markTutorialDone } = require('../controllers/user.controller');
 const router = express.Router();
 
 router.post('/login',loginUser);
@@ -8,5 +8,4 @@ router.get('/logout',logoutUser);
 router.get('/check-session',retrieveCookie);
 router.get('/getMyDashboard/:id',getDashboard);
 router.post('/tutorialDone',markTutorialDone);
-router.get('/fetchTeamLevel/:id',fetchLevel)
 module.exports = router;
