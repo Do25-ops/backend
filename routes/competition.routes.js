4380a398cafc22f518503b1afa56dd512b8e896b
Muhammad Hamza Edhi <abdulanis190@gmail.com>
2025-02-18 19:31:21 +0500

    updated user controller

--- routes/competition.routes.js
+++ routes/competition.routes.js
@@ -7,7 +7,7 @@ router.get('/queries/:id',sendQueries)
 router.get('/leaderboardData',sendLeaderboardData)
 router.post('/submitFile',submitQuery);
 router.get('/getSubmissionStatus',fetchStatus);
-router.get('/fetchTeamLevel/:id',fetchLevel)
+router.get('/fetchTeamLevel/:team_id',fetchLevel)
 
 
 module.exports = router;
